a855c1b35ac455ecd2b563ebb2654e50c7895b98
SumithJayasuriya-FBS <44674003+sumithjaya@users.noreply.github.com>
2025-10-17 10:24:10 +0530

    uodated

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -603,7 +603,7 @@ export default function Home() {
                     xmlns="http://www.w3.org/2000/svg"
                     fill="#000000"
                   >
-                    <g id="SVGRepo_bgCarrier" stroke-width="0"></g>
+                    <g id="SVGRepo_bgCarrier" strokeWidth="0"></g>
                     <g
                       id="SVGRepo_tracerCarrier"
                       stroke-linecap="round"
